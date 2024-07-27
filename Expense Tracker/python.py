749ffa37adac82c3810b6d0cdd902e948e5ed577
Giorgi <giorgikarchiladze5@gmail.com>
2024-07-27 21:23:24 +0400

    Cleaned up the code

--- Expense Tracker/python.py
+++ Expense Tracker/python.py
@@ -2,12 +2,15 @@
 def add_expense(expenses, amount, category):
     expenses.append({'amount': amount, 'category': category})
 
+
 def print_expenses(expenses):
     for expense in expenses:
         print(f'Amount: {expense["amount"]}, Category: {expense["category"]}')
 
+
 def total_expenses(expenses):
     return sum(map(lambda expense: expense['amount'], expenses))
+
 
 def filter_expenses_by_category(expenses, category):
     return filter(lambda expense: expense['category'] == category, expenses)
@@ -46,4 +49,3 @@ def main():
         elif choice == '5':
             print('Exiting the program.')
             break
-
